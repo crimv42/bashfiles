444b40ba48bb02123103b5a2ec81dadd77639f66
Daniel Nelems <daniel.nelems@gmail.com>
2023-09-13 00:31:42 -0600

    add functions

--- functions.bash
+++ functions.bash
@@ -10,3 +10,9 @@ decodecert(){ openssl x509 -in $1 -text -noout; }
 kubefwd(){ k port-forward svc/$1 $2:$( if [ ! -z "$3" ]; then echo $3; else echo $2; fi); }
 headerchk() { curl -s -D - -o /dev/null $1; }
 tlscheck() { nmap --script ssl-enum-ciphers -p 443 $1; }
+
+#K8s functions
+kns() { kubectl config set-context --current --namespace=$1; }
+
+# Mount function
+mountDS1() { sudo mount -t nfs 192.168.0.22:/datastore_1 mount/datastore_1 }
